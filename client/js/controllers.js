241a60d0beb22b6bf73fed0acf6e2fbfd34159dd
Starckk <Starckk>
2016-06-16 10:36:42 -0500

    4th

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -75,7 +75,7 @@ todoApp.controller('TodoCtrl', function($rootScope, $scope, todosFactory) {
     alert(i);
    
 
-    todosFactory.blogTodo($scope.todos[i]._id).then(function(data) {
+    todosFactory.deleteTodo($scope.todos[i]._id).then(function(data) {
       if (data.data) {
         $scope.todos.splice(i, 1);
       }
