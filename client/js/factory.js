3db1f3cf4fd9c1f7cf394e9969fc3001abad8918
Starckk <Starckk>
2016-06-16 14:11:17 -0500

    5th

--- client/js/factory.js
+++ client/js/factory.js
@@ -20,8 +20,9 @@ todoApp.factory('todosFactory', function($http) {
 
 //kps 04/06/16
    _todoService.blogTodo = function(id) {
+    alert(urlBase+ '/' + id);
     return $http.delete(urlBase+ '/' + id);
-    //alert(urlBase+ '/' + id);
+    
   };
 
   return _todoService;
