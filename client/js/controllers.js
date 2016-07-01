71ac4078984e65d64d06576d738c6b22a6a03d8e
Starckk <Starckk>
2016-07-01 12:37:26 -0500

    24

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -10,16 +10,16 @@ todoApp.controller('TodoCtrl', function($rootScope, $scope, todosFactory) {
 
   // Save a Todo to the server
   $scope.save = function($event) {
-    if ($event.which == 13 && $scope.todoInput) {
+    if ($event.which == 13 && $scope.todoInput && $scope.labelInput) {
       todosFactory.saveTodo({
         "todo": $scope.todoInput,
         "isCompleted": false,
-        "label": true
+        "label": $scope.labelInput
       }).then(function(data) {
         $scope.todos.push(data.data);
       });
       $scope.todoInput = '';
-      $scope.choosenLabel = '';
+      $scope.labelInput = '';
       }
   };
 
