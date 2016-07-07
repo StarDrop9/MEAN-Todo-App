0f6505dc1214125a0f7519e6726b7ff15767507b
Starckk <Starckk>
2016-07-07 14:37:12 -0500

    26th

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -66,7 +66,7 @@ todoApp.controller('TodoCtrl', function($rootScope, $scope, todosFactory) {
        "_id": _t._id,
       "todo": $event.target.value.trim(),
        "isCompleted": _t.isCompleted,
-       "label": _t.label
+       "label": label
       }).then(function(data) {
         console.log(label);
        if (data.data.updatedExisting) {
