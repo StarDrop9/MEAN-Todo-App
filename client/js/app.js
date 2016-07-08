f873648ac9e047616d9e19536ff80da3999d0e1c
Starckk <Starckk>
2016-07-08 09:39:43 -0500

    28th

--- client/js/app.js
+++ client/js/app.js
@@ -1,16 +1,22 @@
 todoApp = angular.module('todoApp', ['ngRoute','puElasticInput','monospaced.elastic'])
+
   .config(function($routeProvider) {
+
     $routeProvider
+
       .when('/', {
         templateUrl: '/partials/todo.html',
         controller: 'TodoCtrl'
         })
+      
        .when("/home", { 
         templateUrl: "/partials/homep.html", 
         controller: "homeCtrl"
         }) 
+      
        .otherwise({
         redirectTo: '/'
         });
   });
+
 
