573177cd0d3382ce7f58a246da118c4dc85d8a69
Starckk <Starckk>
2016-07-08 10:30:21 -0500

    28th

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -21,14 +21,13 @@ todoApp.controller('TodoCtrl', function($rootScope, $scope, todosFactory) {
   };
 
  //update the Label of the Todo
-  $scope.updateLabel = function($event, _id, i) {
-    var label = $event.target.value.trim();
-    var _t = $scope.todos[i];
+  $scope.updateLabel = function($event, _id, i,label) {
+       var _t = $scope.todos[i];
    // console.log(label);
     todosFactory.updateTodoLabel({
       _id: _id,
-      label: null,
-      todo: _t.todo
+      "label": label,
+      "todo": _t.todo
     }).then(function(data) {
       if (data.data.updatedExisting) {
         _t.label=label;
