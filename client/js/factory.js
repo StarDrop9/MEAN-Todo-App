74869b41374edfbed3c6e72fe5abb51d7453f9f0
Starckk <Starckk>
2016-07-12 16:13:17 -0500

    34

--- client/js/factory.js
+++ client/js/factory.js
@@ -38,9 +38,8 @@ todoApp.factory('todosFactory', function($http) {
     var todo = $httpParamSerializer({"_id":"56fef2921c91130b00ab4391","todo":"Add Login Piece to this application","isCompleted":false,"label":"Splitting Water"})
       //todo = angular.toJson(todo)
        console.log(todo);
-    return $http.put(urlBase, todo {
-        headers: { 'Content-Type': 'application/json' }
-    });
+    return $http.put(urlBase, todo) 
+    //{headers: { 'Content-Type': 'application/json' } });
   };
 
    _todoService.updateTodo = function(todo) {
