c1c3fc6dd4444fd9d47cd4872be4d3ef8aee130a
Starckk <Starckk>
2016-07-12 11:26:24 -0500

    33

--- client/js/factory.js
+++ client/js/factory.js
@@ -29,8 +29,8 @@ todoApp.factory('todosFactory', function($http) {
     debugger; // Set the debugger inside 
                 // this function
     //{"_id":"56fef2921c91130b00ab4391","todo":"Add Login Piece to this application","isCompleted":false,"label":"Splitting Water"}
-      todo = angular.toJson(todo)
-       console.log(todo);
+     // todo = angular.toJson(todo)
+    //  console.log(todo);
     return  $http.put('api/todos')
     .success(function(todo, status, headers, config) {
      // Do something successful
@@ -40,12 +40,7 @@ todoApp.factory('todosFactory', function($http) {
      // Handle error
      alert("error putting")
      });
-
-
-
-
-
-    $http.put(urlBase, {"_id":"56fef2921c91130b00ab4391","isCompleted":false,"label":"Splitting Water","todo":"Add Login Piece to this application"});
+ 
   };
 
 
