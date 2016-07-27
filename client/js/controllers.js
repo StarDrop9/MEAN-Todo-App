46832b1be8a999f556be2ca45cd5013fdb06374d
Starckk <Starckk>
2016-07-27 17:10:39 -0500

    57

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -12,12 +12,12 @@ todoApp.controller('TodoCtrl', function($rootScope, $scope, todosFactory) {
     $scope.todos = data.data;
   });
 
-  // Save a Todo to the server
+  // Save a Todo to the server removed 07/27PM $event.which == 13 &&
   $scope.save = function($event,label, priority) {
    debugger; // Set the debugger inside 
       console.log(priority);
 
-    if ( $scope.todoInput) {
+    if ( $event.which == 13 && $scope.todoInput) {
       todosFactory.saveTodoTest({
         "todo": $scope.todoInput,
         "isCompleted": false,
