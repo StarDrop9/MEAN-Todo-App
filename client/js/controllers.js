a7f0014e6f368588341dfc49735f927b4ce98c9b
Starckk <Starckk>
2016-08-12 08:56:34 -0500

    1

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -114,15 +114,14 @@ $scope.updateLabelTest = function($event, _id, i, label) {
 
     
       
-    
+     $scope.totalDisplayed = 3;          
     
      $scope.loadMore = function () {
-      
-      $scope.totalDisplayed = 3;          
+      console.log($scope.totalDisplayed );    
       $scope.totalDisplayed += 1;
       console.log($scope.totalDisplayed );   
     // $scope.data = data;
-     console.log($scope.todos);    
+    // console.log($scope.todos);    
     };
     
     
