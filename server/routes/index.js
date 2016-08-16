7063d59c96dc83c95a67abe2b5c3997f29be59cd
Starckk <Starckk>
2016-08-16 15:25:14 -0500

    3

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,8 +8,14 @@
  var db = mongojs('mongodb://Test1:Lance-7531@ds025459.mlab.com:25459/heroku_g7755ftm', ['todos']);
 //var db = mongojs('mongodb://heroku_fdk06q24:4me4grt80flpddgfgj8rcpbv61@ds011820.mlab.com:11820/heroku_fdk06q24', ['todos']);
 
-  /* GET ToDo page. */
+
+
   router.get('/', function(req, res) {
+    res.render('login');
+  });
+
+  /* GET ToDo page. */
+  router.get('/index', function(req, res) {
     res.render('index');
   });
 
