06088cb5c1e45479e4f2d3a63bc26bcfdd2e3e1c
Starckk <Starckk>
2016-08-20 07:20:07 -0500

    5

---
+++ client/js/LoginContoller.js
@@ -0,0 +1,10 @@
+todoApp.controller('LoginCtrl', function($rootScope, $scope,$timeout) {
+ $scope.image = "/pics/greenstar50.png";   
+
+  $scope.setMyVar= function() {
+     debugger;
+        $scope.setMyVar ="Login";
+      };
+     
+
+  });
